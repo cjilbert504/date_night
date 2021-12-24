b624acda14f658ebfca272805e444af0d1312b76
Collin Jilbert <cjilbert504@gmail.com>
2021-12-23 20:45:47 -0600

    Extract previous code into other more focused modules

--- lib/date_night.rb
+++ lib/date_night.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "date_night/version"
+require_relative "date_night/string_formatters"
+require_relative "date_night/time_formatters"
 
+# Module for friendly Time/Date/DateTime formatting
 module DateNight
   class Error < StandardError; end
-  # Your code goes here...
+end
+
+class String
+  include DateNight::StringFormatters
+end
+
+class Time
+  include DateNight::TimeFormatters
 end

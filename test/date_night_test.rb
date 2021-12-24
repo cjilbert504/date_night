2efd1a31ec4a4b30fb577902a47bec859b16fd60
Collin Jilbert <cjilbert504@gmail.com>
2021-12-23 20:46:55 -0600

    Updated tests

--- test/date_night_test.rb
+++ test/date_night_test.rb
@@ -3,7 +3,27 @@
 require "test_helper"
 
 class DateNightTest < Minitest::Test
+  def setup
+    @current_time = Time.now
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::DateNight::VERSION
+  end
+
+  def test_mm_dd_yyyy
+    assert_equal @current_time.mm_dd_yyyy, @current_time.strftime("%m %d %Y")
+  end
+
+  def test_slashed_date_format
+    assert_equal @current_time.mm_dd_yyyy.slashed, @current_time.strftime("%m/%d/%Y")
+  end
+
+  def test_dashed_date_format
+    assert_equal @current_time.mm_dd_yyyy.dashed, @current_time.strftime("%m-%d-%Y")
+  end
+
+  def test_mm_dd_yy
+    assert_equal @current_time.mm_dd_yy, @current_time.strftime("%m %e %y")
   end
 end
